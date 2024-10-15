8d3dbf9d48fc21d01116d39ce51427c6d2aeae9d
Marie Brindöpke <ma871bri@htwg-konstanz.de>
2024-10-15 12:25:55 +0200

    API Rohfassung

--- API/API.py
+++ API/API.py
@@ -1,1 +1,22 @@
-print(f"Theis now running.")
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Beispiel-Endpunkt für GET-Anfragen
+#Um Daten abzurufen z.B. Zustand der LEDs (kommt aber als Info aktuell nicht von der Website
+@app.route('/status', methods=['GET'])
+def get_status():
+    status = {"led1": "off", "led2": "on", "led3": "off"}
+    return jsonify(status)
+
+# Beispiel-Endpunkt für POST-Anfragen
+#Um Daten an den Server zu senden & darauf änderungen vorzunehmen
+@app.route('/trigger-led', methods=['POST'])
+def trigger_led():
+    data = request.get_json()  # Holt die Daten aus der Anfrage
+    led_name = data.get("led")
+
+    return jsonify({"message": f"{led_name} triggered"}), 200
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
